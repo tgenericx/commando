601ee413644e7c4d0a6ec6043939777833810466
Hammed Anuoluwapo Pelumi <hammedanuoluwapopelumi@gmail.com>
2026-02-06 06:15:39 +0000

    chore: reset repo for Rust rewrite

--- src/app/traits.rs
+++
@@ -1,6 +0,0 @@
-use crossterm::event::Event;
-use std::fmt::Debug;
-
-pub trait Handleable: Debug {
-    fn handle_event(&mut self, event: &Event);
-}
--- src/features/welcome_screen/mod.rs
+++
@@ -1,6 +0,0 @@
-/// Module for the welcome screen.
-pub mod content;
-pub mod events;
-pub mod footer;
-pub mod header;
-pub mod ui;

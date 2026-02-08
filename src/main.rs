e43543ce8e39e0f76c6efe2e69203fbd025243a7
Hammed Anuoluwapo Pelumi <hammedanuoluwapopelumi@gmail.com>
2026-02-08 05:37:16 +0000

    refactor: use std::process::ExitCode for exit handling

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use cli::CliController;
 
 fn main() {
     let controller = CliController::new();
-    let exit_code = controller.run();
 
-    std::process::exit(exit_code);
+    controller.run();
 }

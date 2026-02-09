5ec63ee8ab61303a0c899e59d2e73de43b6f7d3a
Hammed Anuoluwapo Pelumi <hammedanuoluwapopelumi@gmail.com>
2026-02-09 18:28:04 +0000

    feat: integrate commit executor into CLI controller workflow

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 mod cli;
+mod commit_executor;
 mod commit_message;
 mod input_collector;
 mod staging_checker;
+
 use cli::CliController;
 use std::process::ExitCode;
 
 fn main() -> ExitCode {
     let controller = CliController::new();
-
     controller.run()
 }
